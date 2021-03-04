f4edbbf5349371432db696084b4a5250e5eacb2b
Fotis <demeasfotis@gmail.com>
2021-03-04 19:26:58 +0200

    1st try for interactive detection plot

--- code/mrcnn/visualize.py
+++ code/mrcnn/visualize.py
@@ -163,8 +163,17 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
-    if auto_show:
-        plt.show()
+    plt.pause(0.001)
+    # if auto_show:
+    #     plt.show()
+
+def make_plot(figsize=(16, 16)):
+    print("Making plot...")
+    _, ax = plt.subplots(1, figsize=figsize)
+    plt.ion()
+    plt.show()
+
+    return ax
 
 
 def display_differences(image,
